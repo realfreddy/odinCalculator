0da49adbe52cec0036b78d8dbb92de05788c5a8f
Freddy <frederickyamah@gmail.com>
2026-01-24 22:15:28 -0600

    added clear button and update display on button click

--- main.js
+++ main.js
@@ -1,32 +1,41 @@
-const display = document.querySelector(".displaynumbers");
-function add(x,y){
-    return x + y;
+const display = document.querySelector(".display-text");
+const buttons = document.querySelector(".buttoncontainer");
+function add(x, y) {
+  return x + y;
 }
-function subtract(x,y){
-    return x - y;
+function subtract(x, y) {
+  return x - y;
 }
-function multiply(x,y){
-    return x * y;
+function multiply(x, y) {
+  return x * y;
 }
-function divide(x,y){
-    return x / y;
+function divide(x, y) {
+  return x / y;
 }
 
 let numberOne;
 let numberTwo;
 let operation;
 
-function operate(numberOne,numberTwo,operation){
-    if (operation === '+'){
-        return add(numberOne,numberTwo)
-    }
-    if (operation === '-'){
-        return subtract(numberOne,numberTwo)
-    } 
-    if (operation === '*'){
-        return multiply(numberOne,numberTwo)
-    } 
-    if (operation === '/'){
-        return divide(numberOne,numberTwo)
-    } 
+function operate(numberOne, numberTwo, operation) {
+  if (operation === "+") {
+    return add(numberOne, numberTwo);
+  }
+  if (operation === "-") {
+    return subtract(numberOne, numberTwo);
+  }
+  if (operation === "*") {
+    return multiply(numberOne, numberTwo);
+  }
+  if (operation === "/") {
+    return divide(numberOne, numberTwo);
+  }
 }
+buttons.addEventListener("click", (event) => {
+  const buttonText = event.target.textContent;
+  if (buttonText === "AC") {
+    display.textContent = "";
+  } else {
+    display.textContent += buttonText;
+  }
+});
