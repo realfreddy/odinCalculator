d8ae1b475ca79fefc6e1454097a557c413f75117
Freddy <frederickyamah@gmail.com>
2026-01-24 19:52:52 -0600

    added basic math functions and operate fucntion

---
+++ main.js
@@ -0,0 +1,31 @@
+function add(x,y){
+    return x + y;
+}
+function subtract(x,y){
+    return x - y;
+}
+function multiply(x,y){
+    return x * y;
+}
+function divide(x,y){
+    return x / y;
+}
+
+let numberOne;
+let numberTwo;
+let operation;
+
+function operate(numberOne,numberTwo,operation){
+    if (operation === '+'){
+        return add(numberOne,numberTwo)
+    }
+    if (operation === '-'){
+        return subtract(numberOne,numberTwo)
+    } 
+    if (operation === '*'){
+        return multiply(numberOne,numberTwo)
+    } 
+    if (operation === '/'){
+        return divide(numberOne,numberTwo)
+    } 
+}
